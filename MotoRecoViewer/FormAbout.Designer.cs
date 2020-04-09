8ccbfef50ef8d45cf4b7edbd4cb11fc583f51591
Schwarze Lanzenreiter <root@schwarze.jp>
2020-04-09 09:04:17 +0900

    Ver1.10 release

--- MotoRecoViewer/FormAbout.Designer.cs
+++ MotoRecoViewer/FormAbout.Designer.cs
@@ -40,9 +40,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Location = new System.Drawing.Point(16, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(214, 27);
+            this.label1.Size = new System.Drawing.Size(265, 34);
             this.label1.TabIndex = 3;
             this.label1.Text = "MotoReco Viewer";
             this.label1.Click += new System.EventHandler(this.FormAbout_Click);
@@ -50,20 +51,22 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(245, 38);
+            this.label2.Location = new System.Drawing.Point(327, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 12);
+            this.label2.Size = new System.Drawing.Size(56, 15);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Ver1.00";
+            this.label2.Text = "Ver1.10";
             this.label2.Click += new System.EventHandler(this.FormAbout_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(15, 72);
+            this.label3.Location = new System.Drawing.Point(20, 90);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(473, 252);
+            this.label3.Size = new System.Drawing.Size(631, 315);
             this.label3.TabIndex = 5;
             this.label3.Text = resources.GetString("label3.Text");
             this.label3.Click += new System.EventHandler(this.FormAbout_Click);
@@ -72,18 +75,20 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(15, 352);
+            this.label4.Location = new System.Drawing.Point(20, 440);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(221, 12);
+            this.label4.Size = new System.Drawing.Size(295, 15);
             this.label4.TabIndex = 6;
             this.label4.Text = "This software using these libraries,";
             // 
             // LinkLabelGMap
             // 
             this.LinkLabelGMap.AutoSize = true;
-            this.LinkLabelGMap.Location = new System.Drawing.Point(15, 364);
+            this.LinkLabelGMap.Location = new System.Drawing.Point(20, 455);
+            this.LinkLabelGMap.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LinkLabelGMap.Name = "LinkLabelGMap";
-            this.LinkLabelGMap.Size = new System.Drawing.Size(54, 12);
+            this.LinkLabelGMap.Size = new System.Drawing.Size(68, 15);
             this.LinkLabelGMap.TabIndex = 7;
             this.LinkLabelGMap.TabStop = true;
             this.LinkLabelGMap.Text = "GMap.Net";
@@ -91,14 +96,15 @@
             // 
             // FormAbout
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(507, 392);
+            this.ClientSize = new System.Drawing.Size(676, 490);
             this.Controls.Add(this.LinkLabelGMap);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormAbout";
             this.Text = "FormAbout";
             this.Click += new System.EventHandler(this.FormAbout_Click);
